e14bafb8aaf6a7d95f31373ee4ef0b1226df36ca
OwenNyo <2301840@sit.singaporetech.edu.sg>
2026-01-23 13:49:31 +0800

    Added respective html pages

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/function1")
+def function1():
+    return render_template("function1.html")
+
+@app.route("/function2")
+def function2():
+    return render_template("function2.html")
+
+@app.route("/function3")
+def function3():
+    return render_template("function3.html")
+
+@app.route("/function4")
+def function4():
+    return render_template("function4.html")
+
+@app.route("/function5")
+def function5():
+    return render_template("function5.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
